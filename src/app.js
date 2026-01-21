86eb55637af2333178914b82b65b67892226137f
Miguel Contreras <mcontrerasjaen@gmail.com>
2026-01-21 11:33:17 +0000

    correcciones

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+
+for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+            console.log(pronoun[i] + adj[a] + noun[n] + '.com' + '.es')
+        }
+    }
+}
+ // console.log("Hello Rigo from the console!");
 };
